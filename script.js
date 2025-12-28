18ba2834a42144f3522ae1838b05d72cd57d1a6b
Rizkykiatz | newbie programmer <canlaikiat@gmail.com>
2025-12-28 18:30:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const consentbox = document.getElementById('container');
+    const accept = document.getElementById('Enter');
+
+   accept.addEventListener('click', function(){
+    consentbox.style.display='none';
+   })
+})
+
+
